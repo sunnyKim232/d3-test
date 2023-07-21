c4f028db257eae7def4fbaf3e24f1cd44edff416
smallstar23 <smallstar232@gmail.com>
2023-07-21 13:59:55 +0900

    circle upgrage

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -1,13 +1,26 @@
-function Circle(){
+import { useEffect, useRef, useState } from "react";
 
-      return (
-        <svg>     
-        <circle
-        cx="150"
-        cy="77"
-        r="40"
-      /></svg>
-      )
+function Circle() {
+  const [dataset, setDataset] = useState(generateDataset());
+
+  const ref = useRef();
+
+  useEffect(() => {
+    const svgElement = d3.select(ref.current);
+    svgElement
+      .selectAll("circle")
+      .data(dataset)
+      .join("circle")
+      .attr("cx", (d) => d[0])
+      .attr("cy", (d) => d[1])
+      .attr("r", 3);
+  }, [dataset]);
+
+  useInterval(() => {
+    const newDataset = generateDataset();
+    setDataset(newDataset);
+  }, 2000);
+  return <svg viewBox="0 0 100 50" ref={ref}></svg>;
 }
 
 export default Circle;
